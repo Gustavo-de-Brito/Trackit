7ed74985d65bc99b9468063e77f4aca6c9654f1c
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-05-25 19:08:34 -0300

    impede a intecao do usuario no loading do login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,20 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import FormContainer from "./FormContainer";
+import { ThreeDots } from  'react-loader-spinner'
 
 export default function LoginForm() {
   const [ userEmail, setUserEmail ] = useState("");
   const [ userPassword, setUserPassword ] = useState("");
+  const [ canBeChanged, setCanBeChanged ] = useState(true);
+
+  function changeLoadLayout() {
+    setCanBeChanged(!canBeChanged);
+  }
 
   function sendUserData(e) {
     e.preventDefault();
+
+    changeLoadLayout();
 
     const body = {
       email: userEmail,
@@ -14,11 +22,19 @@ export default function LoginForm() {
     }
   }
 
+  function verifyCanInteract(action) {
+    if(canBeChanged) {
+      return action;
+    } else {
+      return (e) => e.preventDefault();
+    }
+  }
+
   return (
-    <FormContainer onSubmit={ sendUserData }>
-      <input value={ userEmail } onChange={ e => setUserEmail(e.target.value) } type="email" placeholder="email" required />
-      <input value={ userPassword } onChange={ e => setUserPassword(e.target.value) } type="password" placeholder="senha" required />
-      <button type="submit">Entrar</button>
+    <FormContainer onSubmit={ verifyCanInteract(sendUserData) } canBeChanged={ canBeChanged } >
+      <input value={ userEmail } onChange={ verifyCanInteract(e => setUserEmail(e.target.value)) } type="email" placeholder="email" required />
+      <input value={ userPassword } onChange={ verifyCanInteract(e => setUserPassword(e.target.value)) } type="password" placeholder="senha" required />
+      <button type="submit">{ canBeChanged ? "Entrar" : <ThreeDots color="#FFFFFF" height={14} width={60} /> }</button>
     </FormContainer>
   );
 }
